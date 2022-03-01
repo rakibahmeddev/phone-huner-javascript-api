6702bf3c9dc66ac61e38077d4026828dafe5e8b2
rakib ahmed <rakibahmed.dev@gmail.com>
2022-03-01 16:00:40 +0600

    handled if relese date not found

--- js/scripts.js
+++ js/scripts.js
@@ -65,35 +65,63 @@ const displayPhoneDetails = (phones) => {
     console.log(phones.mainFeatures.sensors);
     document.getElementById("phone-detail-container").innerHTML = `
     <div class="card h-100 pt-5 pb-3 shadow-sm w-50 mx-auto">
-                <img src="${phones.image}" class="img-fluid mx-auto" alt="..." />
+                <img src="${
+                  phones.image
+                }" class="img-fluid mx-auto" alt="..." />
                     <div class="card-body">
 
                         <h4 class=" text-center">${phones.name}</h4>
 
-                        <h6 class="text-center">Brand: <span class="phone-brand">${phones.brand}</span></h6>
+                        <h6 class="text-center">Brand: <span class="phone-brand">${
+                          phones.brand
+                        }</span></h6>
                         
-                        <p class="text-center">${phones.releaseDate}</p>
+                        <p class="text-center">${
+                          phones.releaseDate
+                            ? phones.releaseDate
+                            : "Release date not found."
+                        }</p>
 
                         <h5 class="text-center mt-5">Main Features</h5>
                         <hr/>
-                        <p class="text-center features-p">Chipset: <span class="normal-text">${phones.mainFeatures.chipSet}</span></p>
+                        <p class="text-center features-p">Chipset: <span class="normal-text">${
+                          phones.mainFeatures.chipSet
+                        }</span></p>
 
-                        <p class="text-center features-p">Display: <span class="normal-text">${phones.mainFeatures.displaySize}</span></p>
+                        <p class="text-center features-p">Display: <span class="normal-text">${
+                          phones.mainFeatures.displaySize
+                        }</span></p>
 
-                        <p class="text-center features-p">Memory: <span class="normal-text">${phones.mainFeatures.memory}</span></p>
+                        <p class="text-center features-p">Memory: <span class="normal-text">${
+                          phones.mainFeatures.memory
+                        }</span></p>
 
-                        <p class="text-center features-p">Storage: <span class="normal-text">${phones.mainFeatures.storage}</span></p>
+                        <p class="text-center features-p">Storage: <span class="normal-text">${
+                          phones.mainFeatures.storage
+                        }</span></p>
 
                         <p class="text-center features-p">Sensors: 
-                        <span class="normal-text">${phones.mainFeatures.sensors[0]}, </span>
+                        <span class="normal-text">${
+                          phones.mainFeatures.sensors[0]
+                        }, </span>
 
-                        <span class="normal-text">${phones.mainFeatures.sensors[1]}, </span>
+                        <span class="normal-text">${
+                          phones.mainFeatures.sensors[1]
+                        }, </span>
 
-                        <span class="normal-text">${phones.mainFeatures.sensors[2]}, </span>
+                        <span class="normal-text">${
+                          phones.mainFeatures.sensors[2]
+                        }, </span>
 
-                        <span class="normal-text">${phones.mainFeatures.sensors[3]}, </span>
-                        <span class="normal-text">${phones.mainFeatures.sensors[4]}, </span>
-                        <span class="normal-text">${phones.mainFeatures.sensors[5]} </span>
+                        <span class="normal-text">${
+                          phones.mainFeatures.sensors[3]
+                        }, </span>
+                        <span class="normal-text">${
+                          phones.mainFeatures.sensors[4]
+                        }, </span>
+                        <span class="normal-text">${
+                          phones.mainFeatures.sensors[5]
+                        } </span>
                         </p>
 
                         <a href="#" class="btn btn-primary d-block w-25 mx-auto mt-4">Details</a>
