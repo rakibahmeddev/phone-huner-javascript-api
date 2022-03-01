f0ebdbe518ecbebbba5c07c5c31ffb04b58632b5
rakib ahmed <rakibahmed.dev@gmail.com>
2022-03-01 10:15:08 +0600

    display mobile image, mobile title, mobile brand this all are done

--- js/scripts.js
+++ js/scripts.js
@@ -12,16 +12,16 @@ const displaySearchResutls = (results) => {
     const SearchResultContainer = document.getElementById("search-results");
 
     for (const result of results) {
-        // console.log(result.image);
+        console.log(result.brand);
         const colDiv = document.createElement("div");
         colDiv.classList.add("col");
         colDiv.innerHTML = `
-             <div class="card h-100 pt-5 pb-3">
+             <div class="card h-100 pt-5 pb-3 shadow-sm">
                 <img src="${result.image}" class="img-fluid mx-auto" alt="..." />
                     <div class="card-body">
-                        <h5 class="card-title text-center">Card title</h5>
-                        <p class="card-text text-center">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <button class="btn btn-primary d-block mx-auto" type="btn">Details</button>
+                        <h5 class=" text-center">${result.phone_name}</h5>
+                        <h6 class="text-center">Brand: <span class="phone-brand">${result.brand}</span></h6>
+                        <button class="btn btn-primary d-block mx-auto mt-4" type="btn">Details</button>
                     </div>
             </div>
         `;
